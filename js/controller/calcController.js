34e25a01a82a32953b78c330eabddccf8f0267b6
Silvestre <silvestrevideoproducoes@gmail.com>
2023-09-07 14:50:52 -0300

    start calculator

---
+++ js/controller/calcController.js
@@ -0,0 +1,41 @@
+class calcController {
+  
+  constructor() {
+
+    this._displayCalc = "0";
+    this.currentDate;
+    this.initialize();
+  }
+
+
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "07/09/2023";
+        timeEl.innerHTML = "14:39";
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
+}
+
+

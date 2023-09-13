4df372d325d0cc60035b2fbb8a3812e1df469d53
Silvestre <silvestrevideoproducoes@gmail.com>
2023-09-12 21:27:31 -0300

    treating operators and starting on the display

--- js/controller/calcController.js
+++ js/controller/calcController.js
@@ -84,12 +84,25 @@ isOperator(value){
         let result = eval(this._operation.join(""));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     }
 
     
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >= 0; i--){
+
+            if(!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+
+        }
+
+        this.displayCalc = lastNumber
 
     }
 
@@ -108,6 +121,9 @@ addOperation(value){
         }else {
 
                 this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
+
             }
                
 

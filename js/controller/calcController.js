71ed520c980f5141dbd62e22bdc7287fb6daca78
Silvestre <silvestrevideoproducoes@gmail.com>
2023-09-18 14:57:58 -0300

    project completion

--- js/controller/calcController.js
+++ js/controller/calcController.js
@@ -2,6 +2,8 @@ class calcController {
   
   constructor() {
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -13,8 +15,36 @@ class calcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyBoard();
   }
 
+  pasteFromClipboard(){
+
+    document.addEventListener('paste', e=>{
+
+        let text = e.clipboardData.getData('text');
+
+        this.displayCalc = parseFloat(text);
+
+    })
+  }
+
+
+  copyToClipboard() {
+
+    let input = document.createElement("input");
+
+    input.value = this.displayCalc;
+
+    document.body.appendChild(input);
+
+    input.select();
+
+    document.execCommand("Copy");
+
+    input.remove();
+
+  }
 
 
     initialize(){
@@ -27,8 +57,95 @@ class calcController {
     }, 1000);
 
     this.setLastNumberToDisplay();
+    this.pasteFromClipboard();
+
+    document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+        btn.addEventListener('dblclick', e=>{
+
+            this.toggleAudio();
+
+        });
+
+    });
 
 }
+
+toggleAudio(){
+
+    this._audioOnOff = !this._audioOnOff;
+
+}
+
+playAudio(){
+
+    if (this._audioOnOff) {
+
+        this._audio.currentTime = 0;
+        this._audio.play();
+
+    }
+
+}
+
+
+initKeyBoard(){
+
+    document.addEventListener('keyup', e=> {
+
+        this.playAudio();
+
+        switch (e.key) {
+    
+                case 'Escape':
+                    this.clearAll();
+                    break;
+            
+                case 'Backspace':
+                this.clearEntry();
+                break;
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+        
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            this.addOperation(parseInt(e.key));
+                            break;
+
+                        case 'c':
+                            if(e.ctrlKey) this.copyToClipboard();
+                            break;
+            
+                    }
+
+        });
+        
+}
+
 
 addEventListenerAll(element, events, fn){
 
@@ -91,8 +208,18 @@ pushOperation(value){
 
 
     getResult(){
-
+        try{
+        
         return eval(this._operation.join(""));
+
+    } catch(e){
+        setTimeout(()=>{
+            this.setError();
+        },1);
+
+
+    }
+
     }
 
     calc(){
@@ -247,6 +374,8 @@ addDot(){
 
     execBtn(value){
 
+        this.playAudio();
+
         switch (value){
 
             case 'ac':
@@ -363,8 +492,18 @@ addDot(){
     }
 
     set displayCalc(value){
-        this._displayCalcEl.innerHTML = value;
+
+        if(value.length > 10){
+
+        this.setError();
+
+    return false;
+
     }
+
+    this._displayCalcEl.innerHTML = value;
+    
+}
 
     get currentDate(){
         return new Date();
